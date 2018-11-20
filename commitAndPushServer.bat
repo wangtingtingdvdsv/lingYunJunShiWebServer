0c392fad4088ff505c9ce07c9b91b1103ce670f9
王婷婷 <2372454518@qq.com>
2018-11-20 19:05:50 +0800

    持续更新中

---
+++ commitAndPushServer.bat
@@ -0,0 +1,15 @@
+o git add .
+git add .
+echo "*************************"
+echo git commit -m "持续更新中"
+git commit -m "持续更新中"
+echo "***************************"
+echo git push origin master
+git push origin master
+echo "***************************"
+echo git status
+git status
+
+
+ssh root@wangtingting.top
+weile520
